f46438043ed3c501cccd52dc7f932854f2dea03d
FrostFoe <frostfoe@gmail.com>
2025-07-04 10:15:53 +0000

    > nextn@0.1.0 build > next build
    > Build error occurred
    Error: Configu

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,5 @@
-import type {NextConfig} from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  transpilePackages: ['three'],
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
+/** @type {import('next').NextConfig} */
+const config = {};
 
-export default nextConfig;
+export default config;
